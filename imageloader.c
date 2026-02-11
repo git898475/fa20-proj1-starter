fda4b9b1db2384a86dd39485e0676e28e1e46cf4
git898475 <s64824910@gmail.com>
2026-02-11 22:11:59 +0800

    proj1A finished!

--- imageloader.c
+++ imageloader.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **              Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				ZQX - YOUR NAME HERE
 **
 **
 ** DATE:        2020-08-15
@@ -25,6 +25,60 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
+	FILE *fp = fopen(filename,"r");
+	if (fp == NULL)
+	{
+		perror("ERROR: cannot open the file!\n");
+		return NULL;
+	}
+	Image *new_image = (Image *)malloc(sizeof(Image));
+	if (new_image == NULL)
+	{
+		perror("ERROR: cannot malloc a new Image!\n");
+		return NULL;
+	}
+
+	// read the file type
+	char file_type[5];
+	fscanf(fp, "%s", file_type);
+	//read the row and cols
+	fscanf(fp, "%d %d", &new_image->rows, &new_image->cols);
+	// read the max value
+	int scale;
+	fscanf(fp, "%d", &scale);
+	//creat a new color array according to rows and cols
+	//use two dimensions version(also can use one dimension version)
+	Color **image = (Color **) malloc(new_image->rows * sizeof(Color *));
+	if (image == NULL)
+	{
+		perror("ERROR: cannot malloc a new image!\n");
+		free(new_image);
+		return NULL;
+	}
+	for (uint8_t i = 0; i < new_image->rows;i++){
+		image[i] = (Color *)malloc(new_image->cols * sizeof(Color));
+		if (image[i] == NULL)
+		{
+			for (uint8_t j = 0; j < i;j++){
+				free(image[j]);
+			}
+			free(image);
+			free(new_image);
+			printf("%dth Color array:", i);
+			perror("the Color array point cannot be malloc!\n");
+			return NULL;
+		}
+	}
+	for (uint8_t i = 0; i < new_image->rows; i++){
+		for (uint8_t j = 0; j < new_image->cols; j++)
+		{
+			fscanf(fp, "%hhd %hhd %hhd", &image[i][j].R, &image[i][j].G, &image[i][j].B);
+		}
+	}
+	new_image->image = image;
+	fclose(fp);
+	return new_image;
+
 	//YOUR CODE HERE
 }
 
@@ -32,10 +86,27 @@ Image *readData(char *filename)
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n",image->rows,image->cols);
+	printf("255\n");
+	for (uint8_t i = 0; i < image->rows;i++){
+		for (uint8_t j = 0; j < image->cols; j++){
+			printf("%3d %3d %3d",image->image[i][j].R,image->image[i][j].G,image->image[i][j].B);
+			if (j != image->cols - 1){
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+	for (uint8_t i = 0; i < image->rows;i++){
+		free(image->image[i]);
+	}
+	free(image->image);
+	free(image);
 }
--- steganography.c
+++ steganography.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **				Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				ZQX YOUR NAME HERE
 **
 ** DATE:        2020-08-23
 **
@@ -21,13 +21,66 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
+	Color *new_color = (Color *) malloc(sizeof(Color));
+	if (new_color == NULL){
+		perror("cannot malloc a new color!\n");
+		return NULL;
+	}
 	//YOUR CODE HERE
+	if ((image->image[row][col].B & 1) == 0){
+		new_color->R = 0;
+		new_color->G = 0;
+		new_color->B = 0;
+	} else{
+		new_color->R = 255;
+		new_color->G = 255;
+		new_color->B = 255;
+	}
+	return new_color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	if (image == NULL || image->image == NULL)
+	{
+		return NULL;
+	}
+	Image *new_Image = (Image *)malloc(sizeof(Image));
+	if(new_Image == NULL){
+		perror("cannot alloc a new Image!\n");
+		return NULL;
+	}
+	new_Image->rows = image->rows;
+	new_Image->cols = image->cols;
+	Color **new_image = (Color **)malloc(image->rows * sizeof(Color *));
+	if(new_image == NULL){
+		perror("cannot alloc a new image!\n");
+		free(new_Image);
+		return NULL;
+	}
+	for (uint8_t i = 0; i < image->rows;i++){
+		new_image[i] = (Color *)malloc(image->cols * sizeof(Color));
+		if (new_image[i] == NULL){
+			for (uint8_t j = 0; j < i;j++){
+				free(new_image[j]);
+			}
+			free(new_image);
+			free(new_Image);
+			return NULL;
+			
+		}
+	}
+	for (uint8_t i = 0; i < image->rows;i++){
+		for (uint8_t j = 0; j < image->cols;j++){
+			Color *new_color = evaluateOnePixel(image, i, j);
+			new_image[i][j] = *new_color;
+			free(new_color);
+		}
+	}
+	new_Image->image = new_image;
+	return new_Image;
 }
 
 /*
@@ -46,4 +99,21 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if(argc < 2){
+		return -1;
+	}
+	Image *image = readData(argv[1]);
+	if (image == NULL)
+	{
+		return -1;
+	}
+	Image *steganography_image = steganography(image);
+	if (steganography_image == NULL)
+	{
+		return -1;
+	}
+	writeData(steganography_image);
+	freeImage(image);
+	freeImage(steganography_image);
+	return 0;
 }
